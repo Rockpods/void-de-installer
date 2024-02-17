217bd49feffff5de9d7b7129dea7f23570f63c1f
Rockpods <69652659+Rockpods@users.noreply.github.com>
2024-02-17 23:09:40 +0000

    Finish work on suppporting GNOME

--- README.md
+++ README.md
@@ -1,5 +1,22 @@
-# void-installer
-## WORK IN PROCESS. IF YOU HAVE AN ISSUES PLEASE REPORT THEM AS I ONLY HAVE TWO DEVICES TO TEST THIS SCRIPT ON.
-### NOT AFFILIATED WITH VOID LINUX
+## Void Linux Desktop Environment Installer
 
+<b>NOT AFFILIATED WITH THE VOID LINUX PROJECT.</b>\
 An install script for Void Linux that installs GNOME, and sets it up for day-to-day use.
+
+### Installation Instructions:
+
+NOTE: The script only installs Intel graphics drivers by default at the moment. If you have a different GPU, replace the packages installed in line 19 with the recommended packages for your GPU, which can be found in the [Void Linux Documentation](https://docs.voidlinux.org/config/graphical-session/graphics-drivers/index.html).
+1. Install Void Linux using the `void-installer` script included in the ISO. Install from network source, or else you will have to run a few extra commands on some occasions.
+2. Boot into Void Linux and sign in as any user.
+3. Run `xbps-install -S git` as root.\
+    NOTE: If you have an issue running this command, try updating XBPS by running `xbps-install -u xbps` as root.
+4. Download and run the installer by running the following commands:\
+    ```sh
+    git clone --depth=1 https://github.com/Rockpods/void-de-installer
+    cd void-de-installer
+    chmod u+x install.sh
+    sudo ./install.sh
+    ```
+    If you want to view the script before running, run the following command: `cat install.sh | less`. Press q to exit less.\
+    <b>OPTIONS:</b>\
+    Non-free software: You will be prompted if you want to enable nonfree repos to enable nonfree softare. If you do not know what this is, I suggest pressing pressing `Y` when prompted to enable this. Press `N` to keep nonfree repos disabled.
--- install.sh
+++ install.sh
@@ -1,28 +1,43 @@
 #!/bin/sh
 
-# Add audio and optional nice-to-have apps. Also, remove the default video app and replace it with MPV.
-# Also, install fonts and shit. After I get GNOME working good with some good options and defaults, I will start working on a lightweight DE(enlightenment is what I am most considering at the moment).
-# I might attempt a TUI package manager at one point just to see if I can do it
+# I am considering replacing the default video app with MPV.
+# TO-DO: Work on automatically detecting GPU or promping user about what GPU to use so the graphics driver part actually works. Start work on KDE(and install OctoXBPS if using the KDE install) and Enlightenment.
 
-echo "WARNING: This is an unofficial script that is in development. If you have any issues, please report it at https://github.com/Rockpods/void-installer/tree/main"
-#echo "If you are running this script, you should already have an internet connection and already have Void Linux installed."
-#echo "This has only been tested by installing from the the 2023-06-28 glibc live image with void-installer on the LCD Steam Deck. Please be more verbose if reporting issues using a different version of Void."
-echo "ENSURE THE GRAPHICS DRIVERS PART OF THE SCRIPT IS SET FOR YOUR SYSTEM. You will have to manually configure internet using nmtui, which will not automatically open. Press ctrl+c now if you disagree with anything."
-sleep 8s
+echo "This is an unofficial script that is in development. If you have any issues, please report it at https://github.com/Rockpods/void-de-installer/"
+echo "If you are running this script, you should already have an internet connection and already have Void Linux installed."
+echo "This has only been tested by installing from the the 2023-06-28 glibc live image with void-installer on the LCD Steam Deck and an HP laptop. Please be more verbose if reporting issues using a different version of Void."
+echo "You will have to reconfigure your WiFi when you boot into GNOME."
+echo "IMPORTANT: ENSURE THE GRAPHICS DRIVERS PART OF THE SCRIPT IS SET FOR YOUR SYSTEM. Press ctrl+c now if you must stop the install."
+sleep 10s
 
-# Update XBPS & void to continue installation if someone installed from an old ISO without downloading packages from the internet.
-sudo xbps-install -u xbps
-sudo xbps-install -Syu
+# Update XBPS and Void packages to continue installation if someone installed from an old ISO without downloading packages from the internet, and enable extra repositories you will probably want to use at some point.
+xbps-install -u xbps
+xbps-install -Syu
+xbps-install void-repo-nonfree void-repo-multilib void-repo-multilib-nonfree
 
-# Replace this part with the graphics drivers install process for what you are using. Better solution is planned, but I just want something that works at the moment.
-xbps-install linux-firmware-intel mesa-dri vulkan-loader mesa-vulkan-intel intel-video-accel
+# Replace this part with the graphics drivers install process for what you are using.
+xbps-install -y linux-firmware-intel mesa-dri vulkan-loader mesa-vulkan-intel intel-video-accel
 
-xbps-install dbus gnome elogind gdm NetworkManager
+# Install GNOME and important software. Flathub is enabled for GNOME-Software
+xbps-install -y dbus gnome elogind gdm NetworkManager pipewire gnome-software firefox vlc libreoffice pulseaudio
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+# Disable the network management services used in the installer. I have refrained from removing these, at least until I do more testing, to ensure you can still disable NetworkManager and return to WPA_Supplicant or DHCPCD if something goes wrong.
 rm /var/service/dhcpcd
 rm /var/service/wpa_supplicant
+
+# Enabling services required for GNOME to work
 ln -s /etc/sv/dbus /var/service/
 ln -s /etc/sv/elogind /var/service/
 ln -s /etc/sv/NetworkManager /var/service
-ln -s /etc/sv/gdm /var/service/
-sleep 15s
-reboot
+
+# Enabling audio with Pipewire and Pipewire-Pulse. Pipewire is very annoying to setup on Void so I will work on this once I remember how to set up Pipewire on Void in general. Feel free to add to this yourself if you so desire.
+# mkdir -p /etc/pipewire/pipewire.conf.d
+# ln -s /usr/share/examples/wireplumber/10-wireplumber.conf /etc/pipewire/pipewire.conf.d/
+# ln -s /usr/share/examples/pipewire/20-pipewire-pulse.conf /etc/pipewire/pipewire.conf.d/
+
+# Add directories found in Windows user folder to make it easier to move from Windows or easy to use distros
+mkdir ~/Documents ~/Downloads ~/Music ~/Videos ~/Desktop ~/Pictures
+
+# Start GDM service and reboot
+ln -s /etc/sv/gdm /var/service/ && reboot
