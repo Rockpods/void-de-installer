6e24d5c31f2bd7aac6435643434161aea9483045
Rockpods <69652659+Rockpods@users.noreply.github.com>
2024-08-01 08:15:23 -0500

    Update kde.sh
    I forgot to add ark 💀

--- desktops/kde.sh
+++ desktops/kde.sh
@@ -4,7 +4,7 @@
 echo "Starting KDE Plasma 6 install"
 sleep 5s
 # This install command claims to be installing kde5, but if you check the packages in the Void repos, the name for the Plasma 6 meta package is kde5.
-xbps-install -y kde5 xorg elogind dbus pulseaudio alsa-plugins-pulseaudio cronie bluez xdg-desktop-portal kdegraphics-thumbnailers chrony libavdevice libavcodec xorg-server-xwayland ffmpegthumbs xdg-desktop-portal-kde sddm firefox flatpak
+xbps-install -y kde5 xorg elogind dbus pulseaudio alsa-plugins-pulseaudio cronie bluez xdg-desktop-portal kdegraphics-thumbnailers chrony libavdevice libavcodec xorg-server-xwayland ffmpegthumbs xdg-desktop-portal-kde sddm ark firefox flatpak
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 #if [[ "${1,,}" == *"extra"* ]]; then
